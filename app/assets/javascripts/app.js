517559061a3e81c4795183beb69b6f5632845853
Martin Capeletto <martin@entropeer.com>
2016-09-10 19:01:25 -0700

    Cleanup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
     evaluate: /\{\{(.+?)\}\}/g,
   };
 
+  perform_apps_query();
+
+  window.setInterval(function(){
+    perform_apps_query(); 
+  }, 10000);
+
 });
 
 function display_applications(data){
@@ -16,11 +22,15 @@ function display_applications(data){
 
   $(data.application_instances).each(function(i, app){
     if(app.application_summary != null && app.application_summary.apdex_score != null){
-      app.analytics = calculate_analytics(app);
-      app.bounce_rate = parseFloat(getRandomArbitrary(0,50)).toFixed(2);
-      app.daily_users = Math.round(getRandomArbitrary(0,15000));
+
+      app.apdex_score = app.application_summary.apdex_score;
+      app.response_time = app.application_summary.response_time;
+      app.error_rate = parseFloat(app.application_summary.error_rate, 2);
+      app.throughput = app.application_summary.throughput;
+      
       app.status = calculate_status(app);
       app.result = calculate_result(app);
+
       apps_processed.push(app);
     }
   });
@@ -54,24 +64,18 @@ function display_applications(data){
       levelColors:["#C9302C","#F39C12","#27AE60"],
       title: ""
     });
-
   });
 
   $('#applications').fadeIn(500);
-
-  //generate_ga_report('62035048');
 }
 
 function calculate_status(app){
   //class => warn / ok / error
-  var ap_dex = app.application_summary.apdex_score;
-  var response_time = app.application_summary.response_time;
-
   var status = "a_ok";
 
-  if(ap_dex < 0.5){
+  if(app.apdex_score < 0.5 || app.error_rate > 0 || app.throughput == 0){
     status = "a_error";
-  } else if (ap_dex < 0.7){
+  } else if (app.apdex_score < 0.7){
     status = "b_warn";
   } else {
     status = "c_ok";
@@ -84,7 +88,7 @@ function calculate_status(app){
 
 function calculate_result(app){
   
-  var result = getRandomArbitrary(0,100);
+  var result = app.apdex_score * 100;
 
   return result;
 }
@@ -92,86 +96,6 @@ function calculate_result(app){
 // Returns a random number between min (inclusive) and max (exclusive)
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
-}
-
-function calculate_analytics(app){
-  var view_id = "62035048";
-
-  switch(app.host){
-    case "USMIWEBS010":
-      view_id = "62035048";
-    break;
-
-    case "IWHCWEB001":
-      view_id = "2013095";
-    break;
-
-    case "USMIWEBS013":
-      view_id = "62035048";
-    break;
-
-    case "USPAWEBS005":
-      view_id = "2013095";
-    break;
-
-  }
-
-  return view_id;
-}
-
-// Replace with your view ID.
-function generate_ga_report(view_id){
-  var report = {
-    viewId: view_id,
-    dateRanges: [
-      {
-        startDate: 'today',
-        endDate: 'today'
-      }
-    ],
-    metrics: [
-      {
-        expression: 'ga:sessions'
-      },
-      {
-        expression: 'ga:bounceRate'
-      }
-    ]
-  }
-
-  gapi.client.request({
-    path: '/v4/reports:batchGet',
-    root: 'https://analyticsreporting.googleapis.com/',
-    method: 'POST',
-    body: {
-      reportRequests: [
-        report
-      ]
-    }
-  }).then(display_ga_results, console.error.bind(console));
-
-}
-
-function display_ga_results(response) {
-  var formattedJson = JSON.stringify(response.result, null, 2);
-
-  var totals = response.result.reports[0].data.totals[0].values;
-
-  var sessions = totals[0];
-  var bounce_rate = totals[1];
-
-  $('.day-sessions').html(sessions);
-  $('.bounce-rate').html(parseFloat(bounce_rate).toFixed(2));
-}
-
-function gaLoadedProperly(){
-
-  perform_apps_query();
-
-  window.setInterval(function(){
-    perform_apps_query(); 
-  }, 15000);
-
 }
 
 function perform_apps_query(){
